ce800ba9214db925f3143f9d8742a0b9b395ddf3
LimEmrys <160214411+LimEmrys@users.noreply.github.com>
2024-09-04 16:29:42 +0800

    Chess0,1.py

--- Chess0,1.py
+++ Chess0,1.py
@@ -1,10 +1,10 @@
 letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
-positions_white_x = [2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1]
-positions_white_y = ["a", "b", "c", "d", "e", "f", "g", "h", "a", "b", "c", "d", "e", "f", "g", "h"]
+positions_white_y = [2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1]
+positions_white_x = ["a", "b", "c", "d", "e", "f", "g", "h", "a", "b", "c", "d", "e", "f", "g", "h"]
 
-positions_black_x = [7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8]
-positions_black_y = ["a", "b", "c", "d", "e", "f", "g", "h", "a", "b", "c", "d", "e", "f", "g", "h"]
+positions_black_y = [7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8]
+positions_black_x = ["a", "b", "c", "d", "e", "f", "g", "h", "a", "b", "c", "d", "e", "f", "g", "h"]
 
 symbols_white = ["WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP", "WR", "WH", "WB", "WK", "WQ", "WB", "WH", "WR"]
 symbols_black = ["BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP", "BR", "BH", "BB", "BQ", "BK", "BB", "BH", "BR"]
@@ -16,14 +16,14 @@ def render():
         for idx in range(8):
             skip = 0
             for ind in range(16):
-                if positions_white_x[ind] == i + 1 and positions_white_y[ind] == letters[idx]:
+                if positions_white_y[ind] == i + 1 and positions_white_x[ind] == letters[idx]:
                     row = row + symbols_white[ind]
                     row = row + "|"
                     skip = 1
             
             if skip == 0:
                 for ind in range(16):
-                    if positions_black_x[ind] == i + 1 and positions_black_y[ind] == letters[idx]:
+                    if positions_black_y[ind] == i + 1 and positions_black_x[ind] == letters[idx]:
                         row = row + symbols_black[ind]
                         row = row + "|"
                         skip = 1
